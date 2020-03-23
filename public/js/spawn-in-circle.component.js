2350d3041dc357575ce8b9517fc8cf868a591ca4
Jean Mariano <jean.mariano@flatiron.com>
2020-03-22 21:16:34 -0400

    Avatar coords

--- public/js/spawn-in-circle.component.js
+++ public/js/spawn-in-circle.component.js
@@ -10,8 +10,9 @@ AFRAME.registerComponent('spawn-in-circle', {
     var angleRad = this.getRandomAngleInRadians();
     var circlePoint = this.randomPointOnCircle(this.data.radius, angleRad);
     var worldPoint = {x: circlePoint.x + center.x, y: center.y, z: circlePoint.y + center.z};
-    el.setAttribute('position', worldPoint);
-    // console.log('world point', worldPoint);
+    var newPoint = this.randomCoord(0.65, 5.15, -4.45, 0.05)
+    el.setAttribute('position', newPoint);
+    console.log('world point', newPoint);
 
     var angleDeg = angleRad * 180 / Math.PI;
     var angleToCenter = -1 * angleDeg + 90;
@@ -26,7 +27,13 @@ AFRAME.registerComponent('spawn-in-circle', {
 
   randomPointOnCircle: function (radius, angleRad) {
     var x = Math.cos(angleRad)*radius;
-    var y = Math.sin(angleRad)*radius;
-    return {x: x, y: y};
+    var z = Math.sin(angleRad)*radius;
+    return {x: x, y: 0, z: z};
+  },
+
+  randomCoord: function (x0, xf, z0, zf) {
+    var x = Math.random() * (xf - x0 + 1) + x0;
+    var z = Math.random() * (zf - z0 + 1) + z0;
+    return {x: x, y: 1.5, z: z};
   }
 });
